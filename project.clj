8a31f9a0544946fc021570de3c8d890f5611727c
Maurício Szabo <mauricio.szabo@gmail.com>
2017-09-21 18:29:24 -0300

    Changelog, and version on project.clj.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject microscope/schema-logger "0.1.0"
+(defproject microscope/schema-logger "0.0.1"
   :description "A logger with schema, to avoid name-clash with Kibana/Graylog/ElasticSearch"
   :url "github.com/acessocard/microscope-schema-logger"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [microscope "0.1.5"]
                  [prismatic/schema "1.1.6"]]
