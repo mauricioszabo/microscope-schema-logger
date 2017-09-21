2bdee1127d724820635d04b565b74803ff122b53
Maurício Szabo <mauricio.szabo@gmail.com>
2017-09-21 18:28:54 -0300

    Generate logger only once.

--- src/microscope/schema_logger.clj
+++ src/microscope/schema_logger.clj
@@ -10,23 +10,17 @@
                     (s/optional-key :exception) s/Str
                     (s/optional-key :backtrace) s/Str})
 
-(defrecord Logger [cid original-logger-gen validate-fn]
+(defrecord Logger [cid original-logger validate-fn]
   log/Log
   (log [_ message type data]
     (let [normalized-data (-> data
                               (assoc :cid cid :message message :type type)
-                              validate-fn)
-          original-logger (original-logger-gen {:cid cid})]
+                              validate-fn)]
       (log/log original-logger (:message normalized-data) type normalized-data))))
-
-(defn- generate-coercer [schema coercer]
-  (or coercer #(s/validate schema %)))
 
 (defn generator
   ([] (generator {}))
   ([{:keys [logger schema coercer]
      :or {logger log/default-logger-gen, schema LoggerMessage}}]
-   (fn [{:keys [mocked cid]}]
-     (if mocked
-       (->Logger cid #(logger (assoc % :mocked true)) (generate-coercer schema coercer))
-       (->Logger cid logger (generate-coercer schema coercer))))))
+   (fn [{:keys [cid] :as params}]
+     (->Logger cid (logger params) (or coercer #(s/validate schema %))))))
